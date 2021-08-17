06fe7303de46a29bbbe24381c3c6e50bebea239a
UnicusStella <unicusstella@gmail.com>
2021-08-17 13:00:29 +0900

    modify

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -6,11 +6,19 @@ const itemReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       //TODO
+      return Object.assign({},state,{
+        cartItems:[...state.cartItems, action.payload]
+      })
 
       break;
     case REMOVE_FROM_CART:
       //TODO
-
+      let filterItem = state.cartItems.find(el => el.itemId !== action.payload)
+      console.log(filterItem)
+      return Object.assign({},state,{
+        cartItems:state.cartItems.find(el => el.itemId !== action.payload)
+      })
+      // state.notifications.slice(1)
       break;
     case SET_QUANTITY:
       let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
